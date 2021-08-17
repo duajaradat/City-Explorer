79ef9de66bab439d6fe53eb9314af4b6af8e705d
duajaradat <duajaradat164@gmail.com>
2021-08-17 07:35:47 +0300

    feature#1 done

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React from "react";
-import axios from "axios";
+import City from "./City";
+import "./App.css";
+
 
 class App extends React.Component {
   render() {
     return (
       <div>
-        <header>
-          <h1>
-            City Explorer
-          </h1>
-        </header>
+        <h2>City Location App</h2>
+        <City form="data"/>
       </div>
+      
     );
   }
 }
